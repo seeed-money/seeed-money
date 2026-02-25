87923ad370bc1fc05401742965112d6be37206fb
HHJina <jina199234@gmail.com>
2026-02-25 14:44:26 +0900

    🐛 fix : 유저 API통합 및 수정, 토큰 시간 수정

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from users.views import LoginView, MyProfileView, UserManagementViewSet, UserRegisterView
+from users.views import LoginView, UserManagementViewSet, UserRegisterView
 
 # 1. ViewSet을 위한 Router 설정 (관리자용)
 router = DefaultRouter()
@@ -11,7 +11,7 @@ urlpatterns = [
     # 회원가입 (POST /users/register/)
     path("register/", UserRegisterView.as_view(), name="user-register"),
     # 내 프로필 조회 및 수정 (GET, PATCH /users/profile/)
-    path("profile/", MyProfileView.as_view(), name="user-profile"),
+    # path("profile/", MyProfileView.as_view(), name="user-profile"),
     path("login/", LoginView.as_view(), name="user-login"),
     # [관리자,본인 전용]
     # /users/management/ (GET: 목록조회)
