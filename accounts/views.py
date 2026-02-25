4e9a57f0dcd5fb109ba1582a07cb8c630779ffed
gyugyu99 <marsins90@gmail.com>
2026-02-25 13:46:32 +0900

    🐛 fix : api 기능 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import OpenApiParameter, extend_schema  # ⭐️ Swagger 설정을 위해 추가
 from rest_framework import permissions, viewsets
 
 from .models import Account
@@ -7,13 +8,32 @@ from .serializers import AccountSerializer
 class AccountViewSet(viewsets.ModelViewSet):
     # 데이터 변환기(Serializer) 설정
     serializer_class = AccountSerializer
-    # 인증된(로그인한) 사용자만 이 API를 사용할 수 있도록 권한을 제한합니다.
+    # 인증된(로그인한) 사용자만 이 API를 사용할 수 있도록 권한을 제한
     permission_classes = [permissions.IsAuthenticated]
 
+    # ⭐️ Swagger에서 관리자가 이메일로 검색할 수 있는 입력창
+    @extend_schema(parameters=[OpenApiParameter(name="email", description="검색할 유저의 이메일 일부 (관리자 전용)", required=False, type=str)])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
     def get_queryset(self):
-        # 로그인한 사용자가 본인의 계좌만 볼 수 있도록 데이터 범위를 제한합니다.
-        return Account.objects.filter(user=self.request.user)
+        # ⭐️ 관리자(is_staff)라면 전체 계좌를, 일반 유저라면 본인 계좌만 가져오게
+        user = self.request.user
+
+        if user.is_staff:
+            queryset = Account.objects.all()
+
+            # 🔍 이메일 검색 파라미터 확인 (?email=...)
+            email_query = self.request.query_params.get("email")
+            if email_query:
+                # 이메일에 해당 글자가 포함된 유저의 계좌만 필터링 (대소문자 무시)
+                queryset = queryset.filter(user__email__icontains=email_query)
+
+            return queryset
+
+        # 로그인한 사용자가 본인의 계좌만 볼 수 있도록 데이터 범위를 제한
+        return Account.objects.filter(user=user)
 
     def perform_create(self, serializer):
-        # 계좌를 생성할 때, 현재 API를 호출한 유저 정보(self.request.user)를 자동으로 저장합니다.
+        # 계좌를 생성할 때, 현재 API를 호출한 유저 정보(self.request.user)를 자동으로 저장
         serializer.save(user=self.request.user)
--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework import permissions, viewsets
 
 from .models import Transaction
@@ -9,10 +10,29 @@ class TransactionViewSet(viewsets.ModelViewSet):
     # 인증된 사용자만 접근 가능하도록 설정 (로그인 안 하면 안 보임)
     permission_classes = [permissions.IsAuthenticated]
 
+    @extend_schema(parameters=[OpenApiParameter(name="account", description="필터링할 계좌의 ID (숫자)", required=False, type=int)])
+    def list(self, request, *args, **kwargs):
+        # 기본 list 기능을 그대로 쓰겠다!
+        return super().list(request, *args, **kwargs)
+
     def get_queryset(self):
         # ⭐️1. 내 데이터만 보기 기능:현재 로그인한 유저(self.request.user)
-        return Transaction.objects.filter(user=self.request.user).order_by("-transaction_at")
+        user = self.request.user
+
+        # 관리자(is_staff)라면 전체 데이터를, 일반 유저라면 본인 데이터만 가져옴
+        if user.is_staff:
+            queryset = Transaction.objects.all()
+        else:
+            queryset = Transaction.objects.filter(user=user)
+
+        # ⭐️ 특정 계좌의 거래 내역만 필터링 (URL에 ?account=아이디 가 있을 경우)
+        # 예: /api/transactions/?account=5 요청 시 5번 계좌 내역만 필터링됨
+        account_id = self.request.query_params.get("account")
+        if account_id:
+            queryset = queryset.filter(account_id=account_id)
+
+        return queryset.order_by("-transaction_at")
 
     def perform_create(self, serializer):
-        # ⭐️2.현재 로그인된 유저 정보를 모델의 user 필드에 자동으로 넣어줍니다.
+        # ⭐️2.현재 로그인된 유저 정보를 모델의 user 필드에 자동으로 넣어줌
         serializer.save(user=self.request.user)
